8522ef390b1266ea40976ba91a3caff142a4e700
markzuckerberg44 <julian.honores@alumnos.ucn.cl>
2025-04-15 08:26:07 -0400

    set up de prisma (plugin base de datos)

--- emails/welcome.tsx
+++ emails/welcome.tsx
@@ -8,24 +8,24 @@ type WelcomeProps = {
 export default function Welcome({nombre}: WelcomeProps) {
   return (
     <Html>
-      <div style={{width: "500px", height: "500px", backgroundColor: "#e6e6ff"}}>
+      <div style={{width: "500px", height: "500px", backgroundColor: "#28292C"}}>
         <div style={
           { color: "#ffffff", textAlign: "center",
-            height: "70px", backgroundColor: "#0066ff",
+            height: "70px", backgroundColor: "#121212",
             margin: "auto"
            }}>
           <h1 style={{ color: "#ffffff", textAlign: "center", padding: "15px 0"}}>¡Hola {nombre}!</h1>
         </div>
         <div>
-          <h2 style={{color:"#000000", textAlign:"center", paddingTop: "10px"}}>
+          <h2 style={{color:"#ffffff", textAlign:"center", paddingTop: "10px"}}>
             Recibiste un código de verificación!
           </h2>
-          <h3 style={{color:"#000000", textAlign:"center", paddingBottom: "10px"}}>
+          <h3 style={{color:"#ffffff", textAlign:"center", paddingBottom: "10px"}}>
             Por favor ingresalo en la página para verificar tu email.
           </h3>
           <h1 style={
-            { color:"#000000", textAlign:"center", padding: "10px",
-              backgroundColor: "#b3b3b3", width: "110px", margin:"auto"
+            { color:"#ffffff", textAlign:"center", padding: "10px",
+              backgroundColor: "#121212", width: "110px", margin:"auto", borderRadius: "10px",
             }}>XXXXXX
           </h1>
         </div>
